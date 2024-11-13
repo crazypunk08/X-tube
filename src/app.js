ec76335a6ed3723d390aa2e6e856735ad60253f8
crazypunk08 <naren.lakamannavar@gmail.com>
2024-11-13 16:42:34 +0530

    Added comments

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express, { urlencoded } from "express";
-import cors from "cors";
+import cors from "cors";//Server should only listen to requests from only these domains
 import cookieParser from "cookie-parser"; //To handle crud operations on cookies of clients browser
 const app=express();
 app.use(cors({
@@ -8,7 +8,7 @@ app.use(cors({
 }));
 
  app.use(express.json({limit:"20kb"}))  //This enables to handle  JSON related request 
- app.use(express.urlencoded({extended:true}));
+ app.use(express.urlencoded({extended:true,limit:"16kb"}));
  app.use(express.static("public"));
  app.use(cookieParser());
 export default app;
